c706b9295167648b0ad80ada1b2387efe497ee78
wuyu <wuyu@yunify.com>
2020-12-02 18:43:49 +0800

    Add proto files proto/types.proto

--- scripts/generate_go.sh
+++ scripts/generate_go.sh
@@ -9,6 +9,7 @@
 current_path=$(cd "$(dirname "${0}")" || exit 1; pwd)
 cd "${current_path}"/.. || exit 1
 
+MODULE="github.com/DataWorkbench/gproto"
 GOPATH=$(go env GOPATH)
 
 if [ -z "${GOPATH}" ]; then
@@ -23,9 +24,14 @@ fi
 
 # generate code
 for f in proto/*.proto;do
-  protoc -I=. -I="${GOPATH}"/src  --govalidators_out=. --go_out=. --go-grpc_out=. "$f"
+  name=$(echo "${f}" | sed 's@proto/@@; s@.proto@@')
+  package="$(grep 'go_package' ${f}| grep "$MODULE"|awk '{print $NF}')"
+  package=$(echo "${package}"|sed 's/"//g; s/;//g')
+  dir=${package//"$MODULE"/}
+
+  protoc -I=. -I="${GOPATH}"/src  -I=./proto --go_opt=module="${MODULE}" --go-grpc_opt=module="${MODULE}" --govalidators_opt=paths=source_relative --govalidators_out=. --go_out=. --go-grpc_out=. "$f"
+
+  mv -f proto/"${name}".validator.pb.go ".${dir}/"
 done
 
 exit $?
-
-
