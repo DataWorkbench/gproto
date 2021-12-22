3fd80e10b28e73ca510a63c9c84e2872e4556e1a
gx <gxlevi123@163.com>
2021-12-22 14:21:11 +0800

    fix updatesource add space_id

--- pkg/request/request.validator.pb.go
+++ pkg/request/request.validator.pb.go
@@ -608,6 +608,9 @@ func (this *UpdateSource) Validate() error {
 	if !(len(this.SourceId) == 20) {
 		return github_com_mwitkow_go_proto_validators.FieldError("SourceId", fmt.Errorf(`value '%v' must have a length equal to '20'`, this.SourceId))
 	}
+	if !(len(this.SpaceId) == 20) {
+		return github_com_mwitkow_go_proto_validators.FieldError("SpaceId", fmt.Errorf(`value '%v' must have a length equal to '20'`, this.SpaceId))
+	}
 	if !(len(this.Name) > 0) {
 		return github_com_mwitkow_go_proto_validators.FieldError("Name", fmt.Errorf(`value '%v' must have a length greater than '0'`, this.Name))
 	}
