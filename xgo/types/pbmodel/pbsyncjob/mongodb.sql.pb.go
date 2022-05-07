cda7a0381352af860f8d688273db7e11033b1187
shaolei7788 <shaolei@yunify.com>
2022-05-07 11:11:24 +0800

    Update Mongodb generate file

--- xgo/types/pbmodel/pbsyncjob/mongodb.sql.pb.go
+++ xgo/types/pbmodel/pbsyncjob/mongodb.sql.pb.go
@@ -23,3 +23,16 @@ func (t *MongodbSource) Value() (driver.Value, error) {
 	}
 	return json.Marshal(t)
 }
+
+// Scan for implements sql.Scanner (- database/sql).
+func (t *MongodbTarget) Scan(val interface{}) error {
+	return json.Unmarshal(val.([]byte), t)
+}
+
+// Value for implements driver.Valuer (- database/sql/driver)
+func (t *MongodbTarget) Value() (driver.Value, error) {
+	if t == nil {
+		return nil, nil
+	}
+	return json.Marshal(t)
+}
